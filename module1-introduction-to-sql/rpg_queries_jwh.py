96e14ab21e6f20d72a2946da760faf35b46b07e7
johnwesleyharding <johnwesleyharding@users.noreply.github.com>
2019-12-12 02:44:00 -0600

    late progress on part 1

--- module1-introduction-to-sql/rpg_queries_jwh.py
+++ module1-introduction-to-sql/rpg_queries_jwh.py
@@ -4,24 +4,30 @@ import pandas as pd
 
 def query(q, db = sqlite3.connect('rpg_db.sqlite3')):
     cursor = db.cursor()
-    result = cursor.execute(q).fetchall()
+    cursor.execute(q)
+    result = cursor.fetchall()
     cursor.close()
     db.commit()
-    print(result)
+#     print(result)
     return result
 
-# PART 1
+# PART 1 -------------------------------------
 # How many total Characters are there?
-resultc = query('SELECT COUNT (*) FROM charactercreator_character;')
+q = "SELECT COUNT (*)\
+     FROM charactercreator_character;"
+resultc = query(q)
 print(f'{resultc[0][0]} total characters.')
 
 # How many of each specific subclass?
 result = query('SELECT COUNT (*) FROM charactercreator_fighter;')
 print(f'{result[0][0]} fighters.')
+
 result = query('SELECT COUNT (*) FROM charactercreator_cleric;')
 print(f'{result[0][0]} clerics.')
+
 result = query('SELECT COUNT (*) FROM charactercreator_thief;')
 print(f'{result[0][0]} thieves.')
+
 result = query('SELECT COUNT (*) FROM charactercreator_mage;')
 resultn = query('SELECT COUNT (*) FROM charactercreator_necromancer;')
 print(f'{result[0][0]} mages including {resultn[0][0]} necromancers.')
@@ -40,8 +46,9 @@ for i in range(len(result)):
     print(f'Character ID: {i + 1} has {result[i][0]} items.')
 
 # How many Weapons does each character have? (Return first 20 rows)
-
-
+result = query('SELECT cc.character_id as ID, cc.name as person, count(aw.power) as weapons FROM charactercreator_character as cc, charactercreator_character_inventory as cci, armory_weapon as aw WHERE cc.character_id = cci.character_id AND aw.item_ptr_id = cci.item_id GROUP BY cc.character_id ORDER BY weapons DESC LIMIT 20;')
+for i in range(len(result)):
+    print(f'{result[i][1]} has {result[i][2]} items.')
 
 # On average, how many Items does each Character have?
 result = query('SELECT COUNT (*) FROM charactercreator_character_inventory;')
@@ -49,10 +56,33 @@ print(f'{round(result[0][0] / resultc[0][0], 2)} average items per character')
 
 # On average, how many Weapons does each character have?
 
-# print(f'{resultw[0][0] / resultc[0][0]} average weapons per character')
+result = query('SELECT COUNT(*) FROM charactercreator_character_inventory as cci, armory_weapon as aw WHERE cci.item_id = aw.item_ptr_id;')
+print(f'{round(result[0][0] / resultc[0][0], 2)} average weapons per character')
 
 
-# PART 2
+# PART 2 -------------------------------------
 df = pd.read_csv('buddymove_holidayiq.csv')
+
+
+# Open a connection to a new (blank) database file buddymove_holidayiq.sqlite3
+# df.to_sql()
+
+# buddymove_holidayiq.sqlite3
+
+
+# Use df.to_sql (documentation) to insert the data into a new table review in the SQLite3 database
+
+
+
+# Count how many rows you have - it should be 249!
+
+
+
+# How many users who reviewed at least 100 Nature in the category also reviewed at least 100 in the Shopping category?
+
+
+
+# (Stretch) What are the average number of reviews for each category?
+
 
 
